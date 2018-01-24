70ec94570c05e6aaddff9dc3c7c99097e6ea9f85
MPThLee <mpthlee@gmail.com>
2018-01-24 17:30:15 +0900

    Initialize

---
+++ export_save.bat
@@ -0,0 +1,2 @@
+ECHO "EXPORT FROM HKEY_CURRENT_USER\Software\Bennett Foddy\Getting Over It"
+regedit /e %CD%\save.reg "HKEY_CURRENT_USER\Software\Bennett Foddy\Getting Over It"
---
+++ import_save.bat
@@ -0,0 +1,2 @@
+ECHO "IMPORT TO HKEY_CURRENT_USER\Software\Bennett Foddy\Getting Over It"
+regedit /s %CD%\save.reg "HKEY_CURRENT_USER\Software\Bennett Foddy\Getting Over It"
